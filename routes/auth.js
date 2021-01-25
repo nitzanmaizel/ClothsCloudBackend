5e888fa7b2cbd221065ed08a2693fdffa7124b08
Nitzan Maizel <nitzanmaizel@gmail.com>
2021-01-25 10:06:53 +0200

    Auth rothe - Get logg in user

--- routes/auth.js
+++ routes/auth.js
@@ -113,4 +113,18 @@ router.post(
 	}
 );
 
+// @route    GET api/auth
+// @desc     Get logged in user
+// @access   Private
+
+router.get('/auth', getToken, async (req, res) => {
+	try {
+		const user = await User.findOne({ _id: req.user.id });
+		res.json(user);
+	} catch (err) {
+		console.error(err.massage);
+		res.status(500).send('Server error');
+	}
+});
+
 module.exports = router;
